903f4a3afd9de80acaf150c019687b1659356549
Artem Rudenko <rtomrud@gmail.com>
2020-03-25 00:18:47 +0100

    Improve end to end test

--- index.test.js
+++ index.test.js
@@ -1,52 +1,175 @@
 import test from "./node_modules/tape/index.js";
 import averagedPerceptron from "./index.js";
-import shuffle from "./node_modules/array-shuffle/index.js";
 
-test("averaged-perceptron with update() and then predict()", ({
-  equal,
-  end
-}) => {
-  const { predict, update } = averagedPerceptron();
-  const examples = [
-    [{ height: 4, width: 2 }, "slim"],
-    [{ height: 2, width: 4 }, "fat"],
-    [{ height: 1, width: 4 }, "fat"],
-    [{ height: 2, width: 2.1 }, "fat"],
-    [{ height: 2.1, width: 2 }, "slim"],
-    [{ height: 2, width: 1 }, "slim"],
-    [{ height: 1, width: 2 }, "fat"],
-    [{ height: 1, width: 1.1 }, "fat"],
-    [{ height: 1.1, width: 1 }, "slim"],
-    [{ height: 4, width: 1 }, "slim"]
+test("averaged-perceptron with the Iris dataset", ({ equal, end }) => {
+  // Note that the features can be either an object or an array
+  const trainingDataset = [
+    [[5.1, 3.5, 1.4, 0.2], "Iris setosa"],
+    [[4.9, 3.0, 1.4, 0.2], "Iris setosa"],
+    [[4.7, 3.2, 1.3, 0.2], "Iris setosa"],
+    [[4.6, 3.1, 1.5, 0.2], "Iris setosa"],
+    [[5.0, 3.6, 1.4, 0.2], "Iris setosa"],
+    [[5.4, 3.9, 1.7, 0.4], "Iris setosa"],
+    [[4.6, 3.4, 1.4, 0.3], "Iris setosa"],
+    [[5.0, 3.4, 1.5, 0.2], "Iris setosa"],
+    [[4.4, 2.9, 1.4, 0.2], "Iris setosa"],
+    [[4.9, 3.1, 1.5, 0.1], "Iris setosa"],
+    [[5.4, 3.7, 1.5, 0.2], "Iris setosa"],
+    [[4.8, 3.4, 1.6, 0.2], "Iris setosa"],
+    [[4.8, 3.0, 1.4, 0.1], "Iris setosa"],
+    [[4.3, 3.0, 1.1, 0.1], "Iris setosa"],
+    [[5.8, 4.0, 1.2, 0.2], "Iris setosa"],
+    [[5.7, 4.4, 1.5, 0.4], "Iris setosa"],
+    [[5.4, 3.9, 1.3, 0.4], "Iris setosa"],
+    [[5.1, 3.5, 1.4, 0.3], "Iris setosa"],
+    [[5.7, 3.8, 1.7, 0.3], "Iris setosa"],
+    [[5.1, 3.8, 1.5, 0.3], "Iris setosa"],
+    [[5.4, 3.4, 1.7, 0.2], "Iris setosa"],
+    [[5.1, 3.7, 1.5, 0.4], "Iris setosa"],
+    [[4.6, 3.6, 1.0, 0.2], "Iris setosa"],
+    [[5.1, 3.3, 1.7, 0.5], "Iris setosa"],
+    [[4.8, 3.4, 1.9, 0.2], "Iris setosa"],
+    [[5.0, 3.0, 1.6, 0.2], "Iris setosa"],
+    [[5.0, 3.4, 1.6, 0.4], "Iris setosa"],
+    [[5.2, 3.5, 1.5, 0.2], "Iris setosa"],
+    [[5.2, 3.4, 1.4, 0.2], "Iris setosa"],
+    [[4.7, 3.2, 1.6, 0.2], "Iris setosa"],
+    [[4.8, 3.1, 1.6, 0.2], "Iris setosa"],
+    [[5.4, 3.4, 1.5, 0.4], "Iris setosa"],
+    [[5.2, 4.1, 1.5, 0.1], "Iris setosa"],
+    [[7.0, 3.2, 4.7, 1.4], "Iris versicolor"],
+    [[6.4, 3.2, 4.5, 1.5], "Iris versicolor"],
+    [[6.9, 3.1, 4.9, 1.5], "Iris versicolor"],
+    [[5.5, 2.3, 4.0, 1.3], "Iris versicolor"],
+    [[6.5, 2.8, 4.6, 1.5], "Iris versicolor"],
+    [[5.7, 2.8, 4.5, 1.3], "Iris versicolor"],
+    [[6.3, 3.3, 4.7, 1.6], "Iris versicolor"],
+    [[4.9, 2.4, 3.3, 1.0], "Iris versicolor"],
+    [[6.6, 2.9, 4.6, 1.3], "Iris versicolor"],
+    [[5.2, 2.7, 3.9, 1.4], "Iris versicolor"],
+    [[5.0, 2.0, 3.5, 1.0], "Iris versicolor"],
+    [[5.9, 3.0, 4.2, 1.5], "Iris versicolor"],
+    [[6.0, 2.2, 4.0, 1.0], "Iris versicolor"],
+    [[6.1, 2.9, 4.7, 1.4], "Iris versicolor"],
+    [[5.6, 2.9, 3.6, 1.3], "Iris versicolor"],
+    [[6.7, 3.1, 4.4, 1.4], "Iris versicolor"],
+    [[5.6, 3.0, 4.5, 1.5], "Iris versicolor"],
+    [[5.8, 2.7, 4.1, 1.0], "Iris versicolor"],
+    [[6.2, 2.2, 4.5, 1.5], "Iris versicolor"],
+    [[5.6, 2.5, 3.9, 1.1], "Iris versicolor"],
+    [[5.9, 3.2, 4.8, 1.8], "Iris versicolor"],
+    [[6.1, 2.8, 4.0, 1.3], "Iris versicolor"],
+    [[6.3, 2.5, 4.9, 1.5], "Iris versicolor"],
+    [[6.1, 2.8, 4.7, 1.2], "Iris versicolor"],
+    [[6.4, 2.9, 4.3, 1.3], "Iris versicolor"],
+    [[6.6, 3.0, 4.4, 1.4], "Iris versicolor"],
+    [[6.8, 2.8, 4.8, 1.4], "Iris versicolor"],
+    [[6.7, 3.0, 5.0, 1.7], "Iris versicolor"],
+    [[6.0, 2.9, 4.5, 1.5], "Iris versicolor"],
+    [[5.7, 2.6, 3.5, 1.0], "Iris versicolor"],
+    [[5.5, 2.4, 3.8, 1.1], "Iris versicolor"],
+    [[5.5, 2.4, 3.7, 1.0], "Iris versicolor"],
+    [[5.8, 2.7, 3.9, 1.2], "Iris versicolor"],
+    [[6.3, 3.3, 6.0, 2.5], "Iris virginica"],
+    [[5.8, 2.7, 5.1, 1.9], "Iris virginica"],
+    [[7.1, 3.0, 5.9, 2.1], "Iris virginica"],
+    [[6.3, 2.9, 5.6, 1.8], "Iris virginica"],
+    [[6.5, 3.0, 5.8, 2.2], "Iris virginica"],
+    [[7.6, 3.0, 6.6, 2.1], "Iris virginica"],
+    [[4.9, 2.5, 4.5, 1.7], "Iris virginica"],
+    [[7.3, 2.9, 6.3, 1.8], "Iris virginica"],
+    [[6.7, 2.5, 5.8, 1.8], "Iris virginica"],
+    [[7.2, 3.6, 6.1, 2.5], "Iris virginica"],
+    [[6.5, 3.2, 5.1, 2.0], "Iris virginica"],
+    [[6.4, 2.7, 5.3, 1.9], "Iris virginica"],
+    [[6.8, 3.0, 5.5, 2.1], "Iris virginica"],
+    [[5.7, 2.5, 5.0, 2.0], "Iris virginica"],
+    [[5.8, 2.8, 5.1, 2.4], "Iris virginica"],
+    [[6.4, 3.2, 5.3, 2.3], "Iris virginica"],
+    [[6.5, 3.0, 5.5, 1.8], "Iris virginica"],
+    [[7.7, 3.8, 6.7, 2.2], "Iris virginica"],
+    [[7.7, 2.6, 6.9, 2.3], "Iris virginica"],
+    [[6.0, 2.2, 5.0, 1.5], "Iris virginica"],
+    [[6.9, 3.2, 5.7, 2.3], "Iris virginica"],
+    [[5.6, 2.8, 4.9, 2.0], "Iris virginica"],
+    [[7.7, 2.8, 6.7, 2.0], "Iris virginica"],
+    [[6.3, 2.7, 4.9, 1.8], "Iris virginica"],
+    [[6.7, 3.3, 5.7, 2.1], "Iris virginica"],
+    [[7.2, 3.2, 6.0, 1.8], "Iris virginica"],
+    [[6.2, 2.8, 4.8, 1.8], "Iris virginica"],
+    [[6.1, 3.0, 4.9, 1.8], "Iris virginica"],
+    [[6.4, 2.8, 5.6, 2.1], "Iris virginica"],
+    [[7.2, 3.0, 5.8, 1.6], "Iris virginica"],
+    [[7.4, 2.8, 6.1, 1.9], "Iris virginica"],
+    [[7.9, 3.8, 6.4, 2.0], "Iris virginica"],
+    [[6.4, 2.8, 5.6, 2.2], "Iris virginica"]
   ];
-  const maxIterations = 1000;
-  let iteration = 0;
-  while (iteration < maxIterations) {
-    const shuffled = shuffle(examples);
-    shuffled.forEach(([features, actual]) => update(features, actual));
-    iteration += 1;
+  const testDataset = [
+    [[5.5, 4.2, 1.4, 0.2], "Iris setosa"],
+    [[4.9, 3.1, 1.5, 0.1], "Iris setosa"],
+    [[5.0, 3.2, 1.2, 0.2], "Iris setosa"],
+    [[5.5, 3.5, 1.3, 0.2], "Iris setosa"],
+    [[4.9, 3.1, 1.5, 0.1], "Iris setosa"],
+    [[4.4, 3.0, 1.3, 0.2], "Iris setosa"],
+    [[5.1, 3.4, 1.5, 0.2], "Iris setosa"],
+    [[5.0, 3.5, 1.3, 0.3], "Iris setosa"],
+    [[4.5, 2.3, 1.3, 0.3], "Iris setosa"],
+    [[4.4, 3.2, 1.3, 0.2], "Iris setosa"],
+    [[5.0, 3.5, 1.6, 0.6], "Iris setosa"],
+    [[5.1, 3.8, 1.9, 0.4], "Iris setosa"],
+    [[4.8, 3.0, 1.4, 0.3], "Iris setosa"],
+    [[5.1, 3.8, 1.6, 0.2], "Iris setosa"],
+    [[4.6, 3.2, 1.4, 0.2], "Iris setosa"],
+    [[5.3, 3.7, 1.5, 0.2], "Iris setosa"],
+    [[5.0, 3.3, 1.4, 0.2], "Iris setosa"],
+    [[6.0, 2.7, 5.1, 1.6], "Iris versicolor"],
+    [[5.4, 3.0, 4.5, 1.5], "Iris versicolor"],
+    [[6.0, 3.4, 4.5, 1.6], "Iris versicolor"],
+    [[6.7, 3.1, 4.7, 1.5], "Iris versicolor"],
+    [[6.3, 2.3, 4.4, 1.3], "Iris versicolor"],
+    [[5.6, 3.0, 4.1, 1.3], "Iris versicolor"],
+    [[5.5, 2.5, 4.0, 1.3], "Iris versicolor"],
+    [[5.5, 2.6, 4.4, 1.2], "Iris versicolor"],
+    [[6.1, 3.0, 4.6, 1.4], "Iris versicolor"],
+    [[5.8, 2.6, 4.0, 1.2], "Iris versicolor"],
+    [[5.0, 2.3, 3.3, 1.0], "Iris versicolor"],
+    [[5.6, 2.7, 4.2, 1.3], "Iris versicolor"],
+    [[5.7, 3.0, 4.2, 1.2], "Iris versicolor"],
+    [[5.7, 2.9, 4.2, 1.3], "Iris versicolor"],
+    [[6.2, 2.9, 4.3, 1.3], "Iris versicolor"],
+    [[5.1, 2.5, 3.0, 1.1], "Iris versicolor"],
+    [[5.7, 2.8, 4.1, 1.3], "Iris versicolor"],
+    [[6.3, 2.8, 5.1, 1.5], "Iris virginica"],
+    [[6.1, 2.6, 5.6, 1.4], "Iris virginica"],
+    [[7.7, 3.0, 6.1, 2.3], "Iris virginica"],
+    [[6.3, 3.4, 5.6, 2.4], "Iris virginica"],
+    [[6.4, 3.1, 5.5, 1.8], "Iris virginica"],
+    [[6.0, 3.0, 4.8, 1.8], "Iris virginica"],
+    [[6.9, 3.1, 5.4, 2.1], "Iris virginica"],
+    [[6.7, 3.1, 5.6, 2.4], "Iris virginica"],
+    [[6.9, 3.1, 5.1, 2.3], "Iris virginica"],
+    [[5.8, 2.7, 5.1, 1.9], "Iris virginica"],
+    [[6.8, 3.2, 5.9, 2.3], "Iris virginica"],
+    [[6.7, 3.3, 5.7, 2.5], "Iris virginica"],
+    [[6.7, 3.0, 5.2, 2.3], "Iris virginica"],
+    [[6.3, 2.5, 5.0, 1.9], "Iris virginica"],
+    [[6.5, 3.0, 5.2, 2.0], "Iris virginica"],
+    [[6.2, 3.4, 5.4, 2.3], "Iris virginica"],
+    [[5.9, 3.0, 5.1, 1.8], "Iris virginica"]
+  ];
+  const { update, weights } = averagedPerceptron();
+  for (let epoch = 0; epoch < 1000; epoch += 1) {
+    // We should shuffle the training dataset, but we want a deterministic test
+    trainingDataset.forEach(([features, label]) => update(features, label));
   }
 
-  equal(
-    predict({ height: 8, width: 2 }),
-    "slim",
-    'returns "slim" given a height much higher than the width'
+  // Init a new perceptron from the weights obtained during training (optional)
+  const { predict } = averagedPerceptron(weights());
+  const errors = testDataset.reduce(
+    (errors, [features, label]) => errors + (predict(features) !== label),
+    0
   );
-  equal(
-    predict({ height: 2.1, width: 2 }),
-    "slim",
-    'returns "slim" given a height slightly higher than the width'
-  );
-  equal(
-    predict({ height: 2, width: 2.1 }),
-    "fat",
-    'returns "fat" given a height slightly lower than the width'
-  );
-  equal(
-    predict({ height: 2, width: 8 }),
-    "fat",
-    'returns "fat" given a height much lower than the width'
-  );
+  equal(errors < 5, true, "predicts with less than 10% error rate");
   end();
 });
 
@@ -93,7 +216,7 @@ test("averaged-perceptron with correct arguments", ({ deepEqual, end }) => {
   deepEqual(
     averagedPerceptron({ 0: { 0: 1, 1: 2 }, 1: { 0: 3, 1: 4 } }, 1).weights(),
     { 0: { 0: 1, 1: 2 }, 1: { 0: 3, 1: 4 } },
-    "accepts objects as weights and iterations argument"
+    "accepts objects as weights and epochs argument"
   );
   deepEqual(
     averagedPerceptron([
@@ -109,7 +232,7 @@ test("averaged-perceptron with correct arguments", ({ deepEqual, end }) => {
       [3, 4]
     ]).weights(),
     { 0: { 0: 1, 1: 2 }, 1: { 0: 3, 1: 4 } },
-    "accepts arrays and iterations argument"
+    "accepts arrays and epochs argument"
   );
   end();
 });
